7d7c2835516b4b928abb72e8c986705a55ab42b8
Glen Blanchard <glen.blanchard@adgile.media>
2020-12-14 08:18:46 +1000

    days 11, 12, 13a

---
+++ 13.py
@@ -0,0 +1,55 @@
+import math
+
+arrive = 1006726
+busses = [23,'x','x','x','x','x','x','x','x','x','x','x','x',41,'x','x','x','x','x','x','x','x','x',647,'x','x','x','x','x','x','x','x','x','x','x','x','x','x','x','x','x',13,19,'x','x','x','x','x','x','x','x','x',29,'x',557,'x','x','x','x','x',37,'x','x','x','x','x','x','x','x','x','x',17]
+print(arrive)
+earliest = arrive * 2
+ebus = -1
+for bus in busses:
+    if bus == 'x':
+        continue
+    toa = (math.floor(arrive / bus) + 1) * bus
+    print(f'{bus}: {toa}')
+    if toa < earliest:
+        earliest = toa
+        ebus = bus
+
+print(earliest)
+print(earliest - arrive)
+print('Part1:', ebus * (earliest - arrive))
+
+
+
+a = [67, 7,59,61]
+a = [1789,37,47,1889]
+a = busses
+bus_counter = 0
+mul = math.ceil(100000000000000 / a[0])
+diff = 1
+while a[bus_counter + diff] == 'x':
+    diff += 1
+print('diff', diff)
+while True:
+    n = a[bus_counter + diff]
+    val = a[bus_counter] * mul
+    result = (val + diff) / n
+    if result == int(result):
+        #print(f'{result} = {val} + {diff} / {n} @ {bus_counter} {mul} {diff}')
+        found = True
+        inner_diff = diff - 1
+        for other in range(bus_counter+diff, len(a)):
+            inner_diff += 1
+            n_inner = a[bus_counter+inner_diff]
+            if n_inner == 'x':
+                continue
+            result_inner = (val+inner_diff) / n_inner
+            if result_inner != int(result_inner):
+                #print(f'    {result_inner} != ({val} + {inner_diff}) / {n_inner} @ {bus_counter} {mul} {inner_diff}')
+                found = False
+                break
+        if found:
+            print(f'Success Part2: {int(val)}')
+            break
+    mul += 1
+    if mul % 100000 == 0:
+        print(mul, val)
